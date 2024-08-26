b3c7c77e798cd347b15df502743c784585c9a1c5
TowafH <towafh@nycstudents.net>
2024-08-26 13:05:24 -0400

    Update

--- src/axios/interceptors.js
+++ src/axios/interceptors.js
@@ -1,27 +1,33 @@
-import axios from 'axios'
+import axios from 'axios';
 
 const authFetch = axios.create({
-    baseURL: 'https://www.course-api.com',
+  baseURL: 'https://www.course-api.com',
 });
 
 authFetch.interceptors.request.use(
-    (request) => {
+  (request) => {
     request.headers.common['Accept'] = 'application/json';
     console.log('request sent');
     return request;
-}, 
-(error) =>{
+  },
+  (error) => {
     return Promise.reject(error);
-}
+  }
 );
 
 authFetch.interceptors.response.use(
-(response) => {
-    console.log('Recieved Response!');
+  (response) => {
+    console.log('got response');
     return response;
-}, (error) =>{
-    console.log(error.response)
-}
+  },
+  (error) => {
+    console.log(error.response);
+    if (error.response.status === 404) {
+      // do something
+      console.log('NOT FOUND');
+    }
+    return Promise.reject(error);
+  }
 );
 
-export default authFetch
+export default authFetch;
